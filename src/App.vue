439c3d197d6b6008601b398704a4157b6e77aae0
kkw564 <kkw564@naver.com>
2018-08-06 14:16:12 +0900

    make a rebase-3
    make a branch rebase-3 for test rebase

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <div> {{ crocus }} </div>
     <div> {{ branch2 }} </div>
     <div> {{ branch3 }} </div>
+    <div v-if="rebase == 2"> this is rebase2 </div>
+    <div v-if="rebase == 3"> this is rebase3 </div>
   </div>
 </template>
 
@@ -15,9 +17,15 @@ export default {
     return {
       vueTest: 'hello world',
       crocus: 'www.crocus.co.kr',
-      branch2: 'This is branch2'
-      branch3: 'now branch is branch3'
+      branch2: 'This is branch2',
+      branch3: 'now branch is branch3',
+      rebase: 0
     }
+  },
+
+  created () {
+    var d = new Date()
+    this.rebase = (d % 2) + 2
   }
 }
 </script>

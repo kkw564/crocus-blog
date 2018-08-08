c8e8e9162849cf3b75aa5595e2d157a3de7b9e7f
kkw564 <kkw564@naver.com>
2018-08-08 16:26:01 +0900

    Make mainpage
    use particles and typing js

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,89 @@
 <template>
   <div id="app">
-    <div> {{ vueTest }} </div>
-    <div> {{ crocus }} </div>
-    <div> {{ branch2 }} </div>
-    <div> {{ branch3 }} </div>
-    <div v-if="rebase == 2"> this is rebase2 </div>
-    <div v-if="rebase == 3"> this is rebase3 </div>
+    <!-- particles.js container -->
+    <div id="mainpage" v-show="!isMain" >
+      <div id="particles-js" v-show="!isMain" >
+          <div class='console-container' v-show="!isMain" @click="goBlog">
+              <span id='text' v-show="!isMain"></span>
+              <div class='console-underscore' id='console' v-show="!isMain">_
+              </div>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { consoleText } from '../static/js/mainpage.js'
+
 export default {
   name: 'App',
-
   data () {
     return {
-      vueTest: 'hello world',
-      crocus: 'www.crocus.co.kr',
-      branch2: 'This is branch2',
-      branch3: 'now branch is branch3',
-      rebase: 0
+      isMain: false,
+      crocus: 'http://testmyblog.tistory.com/'
     }
   },
-
-  created () {
-    var d = new Date()
-    this.rebase = (d % 2) + 2
+  mounted: function () {
+    this.isMain = false
+    particlesJS.load('particles-js', '../static/json/particlesjs-config.json', function () {
+      console.log('callback - particles.js config loaded')
+    })
+    consoleText(['Think Creatively', 'Solve Flexibly', 'Advance Yourself', 'Have a Dream', 'With Crocus'], 'text', ['white'])
+    // if (document.location.href === this.crocus) {
+    //   this.isMain = false
+    // } else {
+    //   this.isMain = true
+    // }
+  },
+  methods: {
+    goBlog: function () {
+      this.isMain = true
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* ---- reset ---- */
+  body{
+      margin:0;
+      font:normal 75% Arial, Helvetica, sans-serif;
+  }
+  canvas{
+      display: block;
+      vertical-align: bottom;
+  }
+  /* ---- particles.js container ---- */
+  #particles-js{
+      position:absolute;
+      width: 100%;
+      height: 100%;
+      background-color: transparent;
+      background-image: url("./assets/image/starry.jpg");
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: 50% 50%;
+  }
+  .console-container{
+      font-size: 35px;
+      color:white;
+      user-select: none;
+      position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)
+  }
+  .hidden {
+      opacity:0;
+  }
+  .console-underscore {
+      display:inline-block;
+      color:tomato;
+      position:relative;
+      top:-0.14em;
+      left:-8px;
+  }
+  .full-page{
+      width:100%;
+      height:100%;
+      display:absolute;
+  }
 </style>

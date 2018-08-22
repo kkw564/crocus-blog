d05ef22b5897af3217572f5f141dee2036612560
kkw564 <kkw564@naver.com>
2018-08-22 22:59:41 +0900

    Make a route and decide how to view intro vue

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,20 @@
 <template>
   <div id="app">
-    <div id="mainpage" v-show="!isMain" >
-      <div id="particles-js" v-show="!isMain" >
-          <div class='console-container' v-show="!isMain" @click="goBlog">
-              <span id='text' v-show="!isMain"></span>
-              <div class='console-underscore' id='console' v-show="!isMain">_
-              </div>
-          </div>
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { consoleText } from '../static/js/mainpage.js'
-
 export default {
   name: 'App',
-  data () {
-    return {
-      isMain: false,
-      crocus: 'http://www.crocus.co.kr',
-      a: '',
-      b: ''
-    }
-  },
-  created: function () {
-    this.$store.commit('crocus/setConnectTime')
-    console.log('Welcome to Crocus')
-    let connectTime = this.$store.getters['crocus/getConnectTime']
-    console.log('Your connect time is %s', connectTime)
-  },
-  mounted: function () {
-    this.isMain = false
-    particlesJS.load('particles-js', '../static/json/particlesjs-config.json', function () {
-
-    })
-    consoleText(['Think Creatively', 'Solve Flexibly', 'Advance Yourself', 'Have a Dream', 'With Crocus'], 'text', ['white'])
-    // if (document.location.href === this.crocus) {
-    //   this.isMain = false
-    // } else {
-    //   this.isMain = true
-    // }
-  },
-
-  methods: {
-    goBlog: function () {
-      this.isMain = true
-      // this.mixinTest()
-    },
-    mixinTest: function () {
-      this.echoMixin()
-      this.echoMixin('bye')
-      console.log(this.a = this.getCurrentTime())
-      for (var i = 0; i < 2000000000; i++) {}
-      console.log(this.b = this.getCurrentTime())
-      console.log((this.b.getTime() - this.a.getTime()) / 1000)
-    }
-  }
 }
 </script>
 
 <style>
-  /* ---- reset ---- */
-  body{
-      margin:0;
-      font:normal 75% Arial, Helvetica, sans-serif;
-  }
-  canvas{
-      display: block;
-      vertical-align: bottom;
-  }
-  /* ---- particles.js container ---- */
-  #particles-js{
-      position:absolute;
-      width: 100%;
-      height: 100%;
-      background-color: transparent;
-      background-image: url("./assets/image/starry.jpg");
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: 50% 50%;
-  }
-  .console-container{
-      font-size: 35px;
-      color:white;
-      user-select: none;
-      position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)
-  }
-  .hidden {
-      opacity:0;
-  }
-  .console-underscore {
-      display:inline-block;
-      color:tomato;
-      position:relative;
-      top:-0.14em;
-      left:-8px;
-  }
-  .full-page{
-      width:100%;
-      height:100%;
-      display:absolute;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 </style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="mainpage">
+      <div id="particles-js">
+          <div class='console-container' @click="goBlog">
+              <span id='text'></span>
+              <div class='console-underscore' id='console'>_
+              </div>
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { consoleText } from '../../static/js/mainpage.js'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      crocus: 'http://www.crocus.co.kr',
+      a: '',
+      b: ''
+    }
+  },
+  created: function () {
+    this.$store.commit('crocus/setConnectTime')
+    console.log('Welcome to Crocus')
+    let connectTime = this.$store.getters['crocus/getConnectTime']
+    console.log('Your connect time is %s', connectTime)
+  },
+  mounted: function () {
+    particlesJS.load('particles-js', '../static/json/particlesjs-config.json', function () {
+
+    })
+    consoleText(['Think Creatively', 'Solve Flexibly', 'Advance Yourself', 'Have a Dream', 'With Crocus'], 'text', ['white'])
+  },
+
+  methods: {
+    goBlog: function () {
+      window.location.href = 'http://www.crocus.co.kr/341'
+    },
+    mixinTest: function () {
+      this.echoMixin()
+      this.echoMixin('bye')
+      console.log(this.a = this.getCurrentTime())
+      for (var i = 0; i < 2000000000; i++) {}
+      console.log(this.b = this.getCurrentTime())
+      console.log((this.b.getTime() - this.a.getTime()) / 1000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  canvas{
+      display: block;
+      vertical-align: bottom;
+  }
+  /* ---- particles.js container ---- */
+  #particles-js{
+      position:absolute;
+      width: 100%;
+      height: 100%;
+      background-color: transparent;
+      background-image: url("../assets/image/starry.jpg");
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: 50% 50%;
+  }
+  .console-container{
+      font-size: 35px;
+      color:white;
+      user-select: none;
+      position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)
+  }
+  .hidden {
+      opacity:0;
+  }
+  .console-underscore {
+      display:inline-block;
+      color:tomato;
+      position:relative;
+      top:-0.14em;
+      left:-8px;
+  }
+  .full-page{
+      width:100%;
+      height:100%;
+      display:absolute;
+  }
+</style>
